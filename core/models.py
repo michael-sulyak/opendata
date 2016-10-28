ec075c0c5db8b6220900e6d39780a2dbb4119d34
Michael Sulyak <m.sulyak@gmail.com>
2016-10-28 14:46:18 +0700

    fix

--- core/models.py
+++ core/models.py
@@ -11,18 +11,25 @@ class University(models.Model):
 class Direction(models.Model):
     code = models.CharField(max_length=20)
     name = models.TextField()
-    bachelor = models.ForeignKey(University)
-    master = models.ForeignKey(University)
-    specialist = models.ForeignKey(University)
+    bachelor = models.ForeignKey(University, related_name='bachelor')
+    master = models.ForeignKey(University, related_name='master')
+    specialist = models.ForeignKey(University, related_name='specialist')
 
 
 class Rating(models.Model):
     university = models.ForeignKey(University)
     min_rating = models.IntegerField()
     max_rating = models.IntegerField()
-    type = models.CharField(choices=(('national', 'national'), ('international', 'international')))
+    type = models.CharField(
+        choices=(
+            ('national', 'national'),
+            ('international', 'international')
+        ),
+        max_length=20
+    )
 
 
 class Stats(models.Model):
     year = models.IntegerField()
-
+    university = models.ForeignKey(University)
+    percentage = models.IntegerField()
